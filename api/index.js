0cae22d6a28a7f67fbf752acaf9b38a10882366e
Justin-Aw46 <aw.jia.jun.justin@gmail.com>
2024-07-20 15:24:15 +0800

    put index.js into api folder

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose');
-const Champion = require('./models/champion.model');
+const Champion = require('../models/champion.model');
 const app = express()
 app.use(express.json());
 
